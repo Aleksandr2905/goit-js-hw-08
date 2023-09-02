312514e6f169dc9d23864c25025eed3b647b3438
Aleksandr2905 <natalukha@ukr.net>
2023-09-02 22:44:41 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,20 +2,6 @@ import Player from '@vimeo/player';
 
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+ const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-    player.on('eventName', function(data) {
     
-    });
-
-    var callback = function() {};
-
-    player.off('eventName', callback);
