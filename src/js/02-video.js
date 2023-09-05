ae521c2239f8a139ed4186d8f28bd5f30ce5b1e9
Aleksandr2905 <natalukha@ukr.net>
2023-09-05 18:28:01 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    
+};
  
 
+player.on(' timeupdate', onPlay);
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
